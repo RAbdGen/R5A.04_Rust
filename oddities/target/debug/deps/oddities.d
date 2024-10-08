cc9554f780a3c3296f879dc48ae75a380114b37f
Renaud Abdellou--Genestier <renaud.abdellougenestier@etu.unilim.fr>
2024-10-08 15:58:03 +0200

    ajout oddities

---
+++ oddities/target/debug/deps/oddities.d
@@ -0,0 +1,5 @@
+C:\Work\R5A.04_Rust\oddities\target\debug\deps\oddities.exe: src/main.rs
+
+C:\Work\R5A.04_Rust\oddities\target\debug\deps\oddities.d: src/main.rs
+
+src/main.rs:
---
+++ oddities/target/debug/oddities.d
@@ -0,0 +1,1 @@
+C:\Work\R5A.04_Rust\oddities\target\debug\oddities.exe: C:\Work\R5A.04_Rust\oddities\src\main.rs
